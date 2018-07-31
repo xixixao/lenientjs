4e506f9b3666ef2d0d7fcf56366bbfa73df08418
Michal Srb <srb@fb.com>
2018-07-31 23:27:11 +0100

    Update scripts for newer version of prettier using standalone

--- scripts/build-atom-babylon.js
+++ scripts/build-atom-babylon.js
@@ -7,9 +7,6 @@ shell.cd('babel');
 shell.exec('yarn gulp build-babylon');
 shell.cd('../prettier');
 shell.ln('-fs', '../../babel/packages/babylon', 'node_modules/babylon-lenient');
-shell.exec(
-  'yarn rollup -c scripts/build/rollup.parser.config.js ' +
-    '--environment parser:language-js/parser-babylon',
-);
+shell.exec('node ./scripts/build/build.js -- parser-babylon');
 shell.cp('dist/parser-babylon.js', '../atom-lenientjs/dependencies');
 shell.cd('..');
